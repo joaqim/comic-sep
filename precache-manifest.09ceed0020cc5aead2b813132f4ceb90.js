3b0bce6970f2587b4c0da79b663f4f3c41994f98
Joaqim <joaqimp@gmail.com>
2019-09-23 17:12:57 +0200

    Updates

---
+++ precache-manifest.09ceed0020cc5aead2b813132f4ceb90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f89e442666653302ed22f2daabac1e5",
+    "url": "https://joaqim.github.io/comic-sep/index.html"
+  },
+  {
+    "revision": "f8ccfcf7d2a118962215",
+    "url": "https://joaqim.github.io/comic-sep/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "86f677b1061c76114ff9",
+    "url": "https://joaqim.github.io/comic-sep/static/css/main.6fac324c.chunk.css"
+  },
+  {
+    "revision": "f8ccfcf7d2a118962215",
+    "url": "https://joaqim.github.io/comic-sep/static/js/2.1133d4bc.chunk.js"
+  },
+  {
+    "revision": "86f677b1061c76114ff9",
+    "url": "https://joaqim.github.io/comic-sep/static/js/main.04b5c176.chunk.js"
+  },
+  {
+    "revision": "5e66d0efa5b05b420a68",
+    "url": "https://joaqim.github.io/comic-sep/static/js/runtime-main.4508c259.js"
+  }
+]);
